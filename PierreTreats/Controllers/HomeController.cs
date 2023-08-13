5d86b4336a036ff8745e989f6b2516b0b92ad4bd
pier <pierknows@gmail.com>
2023-08-12 20:50:32 -0400

    WIP splash page and home controller

--- PierreTreats/Controllers/HomeController.cs
+++ PierreTreats/Controllers/HomeController.cs
@@ -4,6 +4,16 @@ namespace PierreTreats.Controllers
 {
     public class HomeController : Controller
     {
+        {
+            private readonly TreatsContext _db;
+            private readonly UserManager<ApplicationUser> _userManager;
+
+            public HomeController(UserManager<ApplicationUser> userManager, TreatsContext db)
+            {
+                _userManager = userManager;
+                _db = db;
+            }
+        }
         [HttpGet("/")]
         public ActionResult Index()
         {
